6fe62e8a33fa11cd649475bedcf8adf8cccbf912
los crasy <ramon_duarte.10@gmail.com>
2022-06-15 15:27:11 -0300

    Primeiro commit

---
+++ foo/src/main/java/br/com/ramon/Startup.java
@@ -0,0 +1,13 @@
+package br.com.ramon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
